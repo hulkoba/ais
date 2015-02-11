666bdf4500e311f6901eec33214c4b799ca54392
hulkoba <jacoba.brandner@web.de>
2015-02-11 15:41:46 +0100

    lsa constructor

---
+++ app/src/main/java/com/example/cobi/ais/LSA.java
@@ -0,0 +1,74 @@
+package com.example.cobi.ais;
+
+import android.location.Location;
+import java.lang.reflect.Array;
+
+/**
+ * Created by cobi on 11.02.15.
+ */
+public class LSA {
+    private String name;
+    private Location lsaLocation;
+    private boolean dependsOnTraffic;
+
+    private Array days;
+    private int duration;
+    private int timeFrom;
+    private int timeTo;
+    private int greenFrom;
+    private int greenTo;
+
+    public LSA(String name, Location lsaLocation, boolean dependsOnTraffic, Array days, int duration, int timeFrom, int timeTo, int greenFrom, int greenTo) {
+        this.name = name;
+        this.lsaLocation = lsaLocation;
+        this.dependsOnTraffic = dependsOnTraffic;
+        this.days = days;
+        this.duration = duration;
+        this.timeFrom = timeFrom;
+        this.timeTo = timeTo;
+        this.greenFrom = greenFrom;
+        this.greenTo = greenTo;
+    }
+
+    public LSA(String name, Location lsaLocation, boolean dependsOnTraffic) {
+        this.name = name;
+        this.lsaLocation = lsaLocation;
+        this.dependsOnTraffic = dependsOnTraffic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Location getLsaLocation() {
+        return lsaLocation;
+    }
+
+    public boolean isDependsOnTraffic() {
+        return dependsOnTraffic;
+    }
+
+    public Array getDays() {
+        return days;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getTimeFrom() {
+        return timeFrom;
+    }
+
+    public int getTimeTo() {
+        return timeTo;
+    }
+
+    public int getGreenFrom() {
+        return greenFrom;
+    }
+
+    public int getGreenTo() {
+        return greenTo;
+    }
+}
